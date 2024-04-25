55f19ce9dfe955fe03f234b171ec98a0bf391216
Aman <amanagrawal1782@gmail.com>
2024-04-25 13:58:23 +0530

    "Commit all the chages"

--- program2.py
+++ program2.py
@@ -1,7 +1,31 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        prev_value = 0
+        
+        for char in reversed(s):
+            value = roman_values[char]
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+            prev_value = value
+        
+        return total
+
+# Example usage:
+solution = Solution()
+print(solution.romanToInt("III"))  # Output: 3
+print(solution.romanToInt("XIV"))   # Output: 14
+print(solution.romanToInt("X"))   # Output: 10
+
