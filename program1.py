e8d9f7236048b73b51c7edef65852d375a25712e
Aman <amanagrawal1782@gmail.com>
2024-04-25 13:50:13 +0530

    commit the program1.py

--- program1.py
+++ program1.py
@@ -1,13 +1,21 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-    
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
+                if not stack or mapping[char] != stack.pop():
+                    return False
+            else:
+                return False
+        
+        return len(stack) == 0
 
-
-
-  
-
+# Example usage:
+solution = Solution()
+print(solution.isValid("()"))  
+print(solution.isValid("()[]{}"))  
+print(solution.isValid("(]"))  
